368081a414ad278038cec084e61f4cac3df6ff3e
Ozan Durgut <49796358+ozan956@users.noreply.github.com>
2023-10-24 22:10:11 +0300

    Update testing_od.py

--- testing_od.py
+++ testing_od.py
@@ -1,1 +1,25 @@
-config
+import git
+import os
+
+def push_to_github(repo_path, commit_message, remote_name='origin', branch_name='master'):
+    # Ensure the provided path exists
+    if not os.path.exists(repo_path):
+        raise ValueError(f"Provided path '{repo_path}' does not exist.")
+
+    # Initialize repo object
+    repo = git.Repo(repo_path)
+
+    # Add all changes
+    repo.git.add(A=True)
+
+    # Commit the changes
+    repo.index.commit(commit_message)
+
+    # Push the changes
+    origin = repo.remote(name=remote_name)
+    origin.push(branch_name)
+
+# Example Usage:
+repo_path = '/path/to/your/repo' # Change this to your repository path
+commit_message = 'Added new files' # Change this to your desired commit message
+push_to_github(repo_path, commit_message)
